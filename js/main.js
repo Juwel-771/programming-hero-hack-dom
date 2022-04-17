94558dcb159733b84eb02b479d3487ec70434529
Juwel-771 <juwelah99@gmail.com>
2022-04-17 16:27:50 +0600

    Hacked

--- js/main.js
+++ js/main.js
@@ -6,10 +6,10 @@ function loadMilestone(){
     const milestones = document.querySelector(".milestones");
     
     milestones.innerHTML = `${milestonesData.map(function(milestone){
-        return `<div class="milestone border-b">
+        return `<div class="milestone border-b" id="${milestone._id}">
         <div class="flex">
-          <div class="checkbox"><input type="checkbox" /></div>
-          <div onclick = "openMilestone(this)">
+          <div class="checkbox"><input type="checkbox" onclick = "markMile(this, ${milestone._id})" /></div>
+          <div onclick = "openMilestone(this, ${milestone._id})">
             <p>
               ${milestone.name}
               <span><i class="fas fa-chevron-down"></i></span>
@@ -28,15 +28,55 @@ function loadMilestone(){
 
 }
 
-function openMilestone(milestoneElement){
+function openMilestone(milestoneElement, id){
   const currentPanel = milestoneElement.parentNode.nextElementSibling;
   const shownPanel = document.querySelector(".show");
+  const active = document.querySelector(".active");
+
+  if(active && !milestoneElement.classList.contains("active")){
+    active.classList.remove("active");
+  }
+
+  milestoneElement.classList.toggle("active");
 
   if(!currentPanel.classList.contains("show") && shownPanel)
     shownPanel.classList.remove("show");
 
-    
+
   currentPanel.classList.toggle("show");
+
+  showMilestone(id);
+}
+
+
+function showMilestone(id){
+  const milestoneImage = document.querySelector(".milestoneImage");
+  const title = document.querySelector(".title");
+  const details = document.querySelector(".details");
+
+  milestoneImage.src = milestonesData[id].image;
+  title.innerText = milestonesData[id].name;
+  details.innerText = milestonesData[id].description;
+}
+
+function markMile(checkbox, id) {
+  const doneList = document.querySelector(".doneList");
+  const milestonesList = document.querySelector(".milestones"); 
+  const item = document.getElementById(id);
+
+  if (checkbox.checked) {
+    // mark as done
+    milestonesList.removeChild(item);
+    doneList.appendChild(item);
+  } else {
+    // back to main list
+    milestonesList.appendChild(item);
+    doneList.removeChild(item);
+
+    // task - do the sorting
+    // reload list
+
+  }
 }
 
 loadMilestone();

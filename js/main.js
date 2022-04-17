7b538909adba049e4bfc63024905bb7a10b9a62a
Juwel-771 <juwelah99@gmail.com>
2022-04-17 14:59:30 +0600

    toggle down

---
+++ js/main.js
@@ -0,0 +1,42 @@
+
+const milestonesData = JSON.parse(data).data;
+// load course milestones data
+
+function loadMilestone(){
+    const milestones = document.querySelector(".milestones");
+    
+    milestones.innerHTML = `${milestonesData.map(function(milestone){
+        return `<div class="milestone border-b">
+        <div class="flex">
+          <div class="checkbox"><input type="checkbox" /></div>
+          <div onclick = "openMilestone(this)">
+            <p>
+              ${milestone.name}
+              <span><i class="fas fa-chevron-down"></i></span>
+            </p>
+          </div>
+        </div>
+        <div class="hidden_panel">
+          ${milestone.modules.map(function(module){
+            return `<div class="module border-b">
+            <p>${module.name}</p>
+          </div>`
+          }).join("")}
+        </div>
+      </div>`;
+    }).join("")}`;
+
+}
+
+function openMilestone(milestoneElement){
+  const currentPanel = milestoneElement.parentNode.nextElementSibling;
+  const shownPanel = document.querySelector(".show");
+
+  if(!currentPanel.classList.contains("show") && shownPanel)
+    shownPanel.classList.remove("show");
+
+    
+  currentPanel.classList.toggle("show");
+}
+
+loadMilestone();
